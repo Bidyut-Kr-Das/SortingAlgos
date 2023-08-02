ad072d55ad9e1ddbbd7a9cce6e4fc67b810c3d7a
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-08-02 14:46:56 +0530

    mergeSort

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 > </br>
 > If any error tell me okay 0\*0
 > ##If you are copying the whole repository make sure to delete this readme file.
-> &emsp;&emsp;&emsp **_-Bidyut Kr Das_**
+> &emsp;&emsp;&emsp; **_-Bidyut Kr Das_**
--- mergeSortP.java
+++ mergeSortP.java
@@ -6,10 +6,76 @@ public class mergeSortP {
         System.out.print("Enter the size of array: ");
         int size = sc.nextInt();
         int[] mainArray = new int[size];
-        for(int i = 0;i<mainArray.length;i++){
-            arr[i] = 
+        System.out.println("Enter array elements one by one: ");
+        for (int i = 0; i < mainArray.length; i++) {
+            mainArray[i] = sc.nextInt();
+            System.out.println(mainArray[i]);
+        }
+        mergeSort(mainArray);
+        System.out.println("After sorting the Array: ");
+        for (int elements : mainArray) {
+            System.out.print(elements + " ");
+        }
+        sc.close();
+    }
+
+    /*
+     * mergeSort function which recursively calls itself and breaks the array into
+     * subarrays
+     */
+    static void mergeSort(int[] array) {
+        if (array.length <= 1) // base case where the recursion will stop
+            return;
+        int middle = array.length / 2; // dividing the array
+
+        // creating the left and right sub arrays
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[array.length - middle];
+
+        // putting values of array to sub arrays
+        int i = 0; // traversing left array
+        int j = 0; // traversing right array
+        for (; i < array.length; i++) {
+            if (i < middle) {
+                leftArray[i] = array[i];
+            } else {
+                rightArray[j] = array[i];
+                j++;
+            }
         }
 
-        sc.close();
+        // recursively calling for left and right sub array
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);// this will sort and merge the sub arrays
+
+    }
+
+    static void merge(int[] leftArray, int[] rightArray, int[] array) {
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int i = 0, l = 0, r = 0;
+        while (l < leftSize && r < rightSize) {
+            if (leftArray[l] < rightArray[r]) {
+                array[i] = leftArray[l];
+                i++;
+                l++;
+            } else {
+                array[i] = rightArray[r];
+                r++;
+                i++;
+            }
+
+        }
+        while (l < leftSize) {
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+        while (r < rightSize) {
+            array[i] = rightArray[r];
+            r++;
+            i++;
+        }
     }
 }
